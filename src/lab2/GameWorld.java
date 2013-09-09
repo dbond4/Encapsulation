680ae1b95a6c1b362a9719d515d868c3f1e6322f
F03 H4MM3R <F03 H4MM3R@10.30.30.97>
2013-09-09 15:20:43 -0500

    added 3 methods and get/sets to GameWorld

--- src/lab2/GameWorld.java
+++ src/lab2/GameWorld.java
@@ -9,5 +9,50 @@ package lab2;
  * @author F03 H4MM3R
  */
 public class GameWorld {
-    
+    //these would actually be graphics, not strings!!
+    private String tree;
+    private String house;
+    private String mountain;
+    private String lake;
+
+    public String getTree() {
+        return tree;
+    }
+
+    public void setTree(String tree) {
+        this.tree = tree;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public String getMountain() {
+        return mountain;
+    }
+
+    public void setMountain(String mountain) {
+        this.mountain = mountain;
+    }
+
+    public String getLake() {
+        return lake;
+    }
+
+    public void setLake(String lake) {
+        this.lake = lake;
+    }
+    public void createStorm(){
+        
+    }
+    public void sunRise(){
+        
+    }
+    public void sunSet(){
+        
+    }
 }
