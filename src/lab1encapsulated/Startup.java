76045bcc011f1059d150331128fc3f947bbabc36
F03 H4MM3R <F03 H4MM3R@10.30.30.97>
2013-09-08 20:19:08 -0500

    no new changes

--- src/lab1encapsulated/Startup.java
+++ src/lab1encapsulated/Startup.java
@@ -10,7 +10,9 @@ package lab1encapsulated;
  */
 public class Startup {
     public static void main(String[] args) {
+        
         Employee employee = new Employee();
+        
         employee.completeOrientation();
 
         System.out.println("The employee's status is: " + employee.getStatus());
