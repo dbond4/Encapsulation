5c73ba8fb2e395cfcde85ae709dfe3786e2538c4
F03 H4MM3R <F03 H4MM3R@10.30.30.97>
2013-09-08 20:15:45 -0500

    added main method to the startup for lab2

--- src/lab2/Startup.java
+++ src/lab2/Startup.java
@@ -9,5 +9,9 @@ package lab2;
  * @author F03 H4MM3R
  */
 public class Startup {
-    
+    public static void main(String[] args) {
+        
+        Game game = new Game;
+        game.start();
+    }
 }
